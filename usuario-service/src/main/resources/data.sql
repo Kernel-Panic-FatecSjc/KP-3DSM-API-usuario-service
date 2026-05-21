5a5f226b536349555385f1657f043e895837a40c
Vinícius Leite <contato.vinips25@outlook.com>
2026-05-21 13:15:15 -0300

    fix: Arrumando o insert de usuarios, para ter uma senha para testes

--- usuario-service/src/main/resources/data.sql
+++ usuario-service/src/main/resources/data.sql
@@ -1,8 +1,8 @@
 INSERT INTO usuario (id, nome, email, senha, cargo, ativo, salario, gerente_id, data_criacao, data_atualizacao) 
 VALUES 
-  (1, 'Carlos Mendes', 'carlos.mendes@kernelpanic.com', '$2a$10$...', 'ROLE_GESTOR', true, '8500', NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-  (2, 'Ana Lima', 'ana.lima@kernelpanic.com', '$2a$10$...', 'ROLE_FINANCEIRO', true, '7200', NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-  (3, 'Rafael Costa', 'rafael.costa@kernelpanic.com', '$2a$10$...', 'ROLE_PROFISSIONAL', true, '5500', 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-  (4, 'Juliana Souza', 'juliana.souza@kernelpanic.com', '$2a$10$...', 'ROLE_PROFISSIONAL', true, '5800', 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
+  (1, 'Carlos Mendes', 'carlos.mendes@kernelpanic.com', '$2a$10$W3ZXrhgR5l4qiui8FVIsIOJpwpX5A59Thsr8Ugq2BAvuM5XXTde1W', 'ROLE_GESTOR', true, '8500', NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  (2, 'Ana Lima', 'ana.lima@kernelpanic.com', '$2a$10$W3ZXrhgR5l4qiui8FVIsIOJpwpX5A59Thsr8Ugq2BAvuM5XXTde1W', 'ROLE_FINANCEIRO', true, '7200', NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  (3, 'Rafael Costa', 'rafael.costa@kernelpanic.com', '$2a$10$W3ZXrhgR5l4qiui8FVIsIOJpwpX5A59Thsr8Ugq2BAvuM5XXTde1W', 'ROLE_PROFISSIONAL', true, '5500', 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  (4, 'Juliana Souza', 'juliana.souza@kernelpanic.com', '$2a$10$W3ZXrhgR5l4qiui8FVIsIOJpwpX5A59Thsr8Ugq2BAvuM5XXTde1W', 'ROLE_PROFISSIONAL', true, '5800', 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
 AS new_data
 ON DUPLICATE KEY UPDATE id = new_data.id;
